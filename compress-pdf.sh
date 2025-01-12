7cfd8b8bccc31588b5a32dfe5919ceb6847b740d
chrichri <christophe.brun@papit.fr>
2025-01-12 22:17:20 +0100

    Before first day.

--- compress-pdf.sh
+++ compress-pdf.sh
@@ -5,4 +5,4 @@ if ! [ -x "$(command -v gs)" ]; then
   exit 1
 fi
 # Compress PDF with lossless default parameter
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/default -dNOPAUSE -dQUIET -dBATCH -sOutputFile=out/linux-readable.pdf out/linux.pdf
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/default -dNOPAUSE -dQUIET -dBATCH -sOutputFile=out/frontend-js-readable.pdf out/frontend-js.pdf
